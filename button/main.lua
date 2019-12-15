313b85f49ff494f6f322071e1e2d5d81aab5a931
Noah <sheltielover432@gmail.com>
2019-12-15 00:59:33 -0500

    Set up input

--- button/main.lua
+++ button/main.lua
@@ -5,6 +5,7 @@ function love.load()
   button.size = 50
   score = 0
   timer = 0
+  myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -12,6 +13,17 @@ function love.update(dt)
 end
 
 function love.draw()
+  -- Button --
   love.graphics.setColor(255, 0, 0)
   love.graphics.circle("fill", button.x, button.y, button.size)
+  -- Score --
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.setFont(myFont)
+  love.graphics.print(score)
+end
+
+function love.mousepressed(x, y, b, isTouch)
+  if b == 1 then
+    --body...
+  end
 end
