e9e40b9de20aa017b8042c20dd661040304f2eb2
Noah <sheltielover432@gmail.com>
2019-12-15 00:45:35 -0500

    Added color commands

--- button/main.lua
+++ button/main.lua
@@ -7,6 +7,8 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.rectangle("fill", 0, 0, 200, 100)
-  love.graphics.circle("fill", 100, 100, 100, 100)
+  love.graphics.setColor(0, 0, 255)
+  love.graphics.rectangle("fill", 200, 400, 200, 100)
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.circle("fill", 150, 350, 100, 100)
 end
