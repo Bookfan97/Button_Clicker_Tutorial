69753d9c1c6bea2a901e0300b5eed1486f058235
Noah <sheltielover432@gmail.com>
2019-12-15 01:29:31 -0500

    Main Menu

--- button/main.lua
+++ button/main.lua
@@ -5,23 +5,33 @@ function love.load()
   button.size = 50
   score = 0
   timer = 10
+  gameState = 1
+
   myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
-  -- Timer --
-  if timer > 0 then
-    timer = timer - dt
-  end
-  if timer < 0 then
-    timer = 0
+  if gameState == 2 then
+    -- Timer --
+    if timer > 0 then
+      timer = timer - dt
+    end
+    -- Starting a new gameState --
+    if timer < 0 then
+      timer = 0
+      gameState = 1
+      Score = 0
+      timer = 10
+    end
   end
 end
 
 function love.draw()
-  -- Button --
-  love.graphics.setColor(255, 0, 0)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    -- Button --
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
   -- Score --
   love.graphics.setColor(255, 255, 255)
   love.graphics.setFont(myFont)
@@ -37,6 +47,9 @@ function love.mousepressed(x, y, b, isTouch)
       button.x = math.random(button.size, love.graphics.getWidth()-button.size)
       button.y = math.random(button.size, love.graphics.getHeight()-button.size)
     end
+  end
+  if gameState == 1 then
+    gameState = 2
   end
 end
 
